dc02eeb334cd96437a3b96e875f9852ff853c31e
lab302-15 <lab302-15@labeng.local>
2020-01-07 16:30:42 +0700

    lab5

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-
-
+string mixText(string s1,string s2){
+	int L1 = s1.size();
+	int L2 = s2.size();
+	string s = "";
+	if(L1 != L2){
+		cout << "E";
+	}else{
+		int i = 0;
+			while(i < L1){
+				s = s + s1[i] + s2[i];
+				i++;
+		}
+			return s;
+	}
+		       
+}
 
 int main(){	
 	cout << mixText("AAA","BBB") << "\n";
